1efe3ed8187aa82d5496150540acca0323d77bfe
Ian Dancan Mongare <dancanian25@gmail.com>
2023-09-01 20:51:10 +0300

    initial commit

--- src/ObjectOrientedProgramming/Enum/DaysOfTheWeek.java
+++ src/ObjectOrientedProgramming/Enum/DaysOfTheWeek.java
@@ -5,14 +5,13 @@ public enum DaysOfTheWeek {
     SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
 
     public static void main(String[] args) {
-
         System.out.println(DaysOfTheWeek.SUNDAY);
 
         //iterating through the enum
-
         for (DaysOfTheWeek dd : DaysOfTheWeek.values()){
             System.out.println(dd + " ");
         }
 
+        System.out.println(DaysOfTheWeek.FRIDAY + " is the " + DaysOfTheWeek.FRIDAY.ordinal() + "th day of the week");
     }
 }
