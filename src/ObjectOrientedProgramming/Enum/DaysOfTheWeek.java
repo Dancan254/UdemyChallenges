a6da93c9d1f5fca094cba1a8b4ebda15eda58a18
Ian Dancan Mongare <dancanian25@gmail.com>
2023-09-02 14:04:44 +0300

    update using enum

--- src/ObjectOrientedProgramming/Enum/DaysOfTheWeek.java
+++ src/ObjectOrientedProgramming/Enum/DaysOfTheWeek.java
@@ -9,8 +9,25 @@ public enum DaysOfTheWeek {
 
         //iterating through the enum
         for (DaysOfTheWeek dd : DaysOfTheWeek.values()){
-            System.out.println(dd + " ");
+            //System.out.println(dd + " ");
+            SwitchDayOfWeek(dd);
         }
         System.out.println(DaysOfTheWeek.FRIDAY + " is the " + DaysOfTheWeek.FRIDAY.ordinal() + "th day of the week");
+
+    }
+
+    public static void SwitchDayOfWeek(DaysOfTheWeek weekDay){
+
+        int weekdayInteger = weekDay.ordinal() + 1;
+        switch (weekDay){
+            case SUNDAY -> System.out.println("Sunday is day : " + weekdayInteger);
+            case MONDAY -> System.out.println("Monday is day : " + weekdayInteger);
+            case TUESDAY -> System.out.println("Tuesday is day : " + weekdayInteger);
+            case WEDNESDAY -> System.out.println("Wednesday is day : " + weekdayInteger);
+            case THURSDAY -> System.out.println("Thursday is day : " + weekdayInteger);
+            case FRIDAY -> System.out.println("Friday is day : " + weekdayInteger);
+            case SATURDAY -> System.out.println("Saturday is day : " + weekdayInteger);
+            default -> System.out.println("Day chosen not present");
+        }
     }
 }
