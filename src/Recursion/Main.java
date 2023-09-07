d93879ad811031e088c54e5a43f44fe424a2125a
Ian Dancan Mongare <dancanian25@gmail.com>
2023-09-07 08:37:14 +0300

    Adding iteration  method

--- src/Recursion/Main.java
+++ src/Recursion/Main.java
@@ -8,13 +8,15 @@ public class Main {
         System.out.println("Enter number: ");
 
         int number = sn.nextInt();
-        int sum = Summation(number);
-        System.out.println("The summation of " + number + " is " + sum);
+//        int sum = Summation(number);
+//        System.out.println("The summation of " + number + " is " + sum);
+//
+//        System.out.println("Enter number: ");
+//        int num = sn.nextInt();
+//        System.out.println("Factorial is: " + factorial(num));
+//        counter(4);
 
-        System.out.println("Enter number: ");
-        int num = sn.nextInt();
-        System.out.println("Factorial is: " + factorial(num));
-        counter(4);
+        System.out.println("Using iteration for factorial: " + factor(number));
     }
 
     //method for summation
@@ -48,5 +50,18 @@ public class Main {
             System.out.println(n);
             counter(n - 1);
         }
+    }
+
+    //using iteration for factorial
+    public static int factor(int num){
+        int ans = 1;
+        if (num == 0){
+            return 0;
+        }
+        for (int i = 1; i <= num; i++) {
+
+            ans = ans * i;
+        }
+        return ans;
     }
 }
