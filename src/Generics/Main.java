336f2bad4cd3d16c0055bdec3ff62fcff70306ea
Ian Dancan Mongare <dancanian25@gmail.com>
2023-09-13 14:44:06 +0300

    initial commit

--- src/Generics/Main.java
+++ src/Generics/Main.java
@@ -34,10 +34,31 @@ public class Main {
         var Mickey = new HockeyTeamPlayer("Mickey", "keeper");
         hockey.addTeamMember(Mickey);
         hockey.listTeamMembers();
+
+        System.out.println("Using generic class team");
+        //using generic class Team
+        Team<BaseBallPlayer> baseBallPlayerTeam = new Team<>("DEKut");
+        BaseBallPlayer Duncan = new BaseBallPlayer("Duncan", "Winger");
+        baseBallPlayerTeam.addTeamMember(Duncan);
+        baseBallPlayerTeam.listTeamMembers();
+
+        //football player
+        Team<FootBallPlayer> footBallPlayerTeam = new Team<>("Kimathi");
+        FootBallPlayer Koshy = new FootBallPlayer("Stewart", "RightBack");
+        footBallPlayerTeam.addTeamMember(Koshy);
+        footBallPlayerTeam.listTeamMembers();
     }
 
     public static void scoreResult(BaseBallTeam team1, int t1_Score,
                                    BaseBallTeam team2, int t2_Score){
+
+        String message = team1.setScore(t1_Score, t2_Score);
+        team2.setScore(t2_Score, t1_Score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+
+    public static void scoreResult(Team team1, int t1_Score,
+                                   Team team2, int t2_Score){
 
         String message = team1.setScore(t1_Score, t2_Score);
         team2.setScore(t2_Score, t1_Score);
