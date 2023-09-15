96b843389ffa87f2caa40de95728bafc03154a4f
Ian Dancan Mongare <dancanian25@gmail.com>
2023-09-15 20:37:41 +0300

    Adding Querying interface

---
+++ src/Generics/Model/util/QueryItem.java
@@ -0,0 +1,5 @@
+package Generics.Model.util;
+
+public interface QueryItem {
+    public boolean matchFieldValue(String fieldName, String value);
+}
---
+++ src/Generics/Model/util/QueryList.java
@@ -0,0 +1,9 @@
+package Generics.Model.util;
+
+import java.util.List;
+
+public class QueryList <T extends QueryItem> {
+    private List<T> items;
+
+
+}
